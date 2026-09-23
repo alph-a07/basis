8f818e1b8c718896f16958b083d616975ef2aec8
Jeel Patel <itsjeel01@gmail.com>
2026-09-23 16:40:24 +0530

    chore: initialize Basis project

---
+++ DECISIONS.md
@@ -0,0 +1,8 @@
+# Decisions
+
+## D-1: Library Name
+
+> **Basis**: A stable, simple, configurable design system for Jetpack Compose.
+
+-   The basis is the foundation on which something else is built.
+-   Would look good as a prefix for the API name, e.g., `BasisButton`, `BasisTextField`, etc.
---
+++ basis/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "io.github.alph_a07.basis"
+    group = "io.github.alph_a07"
+
+    version = "0.1.0"
+
+    compileSdk {
+        version = release(37)
+    }
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+}
